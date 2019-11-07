33721ee85fb0fbf2fe3b97b4c025d506be1c4a84
apeplins <57507521+apeplins@users.noreply.github.com>
2019-11-07 18:05:22 -0500

    Update ency.py

--- ency.py
+++ ency.py
@@ -2,11 +2,17 @@
 import subprocess
 
 dir = "demoCA" #where everything is kept
-#Certs_dir = dir/certs
-#crl_dir = dir/crl
-#new_certs_dir = dir/newcerts
-#database = dir/index.txt
-#serial = dir /serial
+Certs_dir = dir + "/" + "certs"
+crl_dir = dir + "/" + "crl"
+new_certs_dir = dir + "/" + "newcerts"
+database = dir + "/" + "index.txt"
+serial = dir + "/" + "serial"
+opensslcnf = "/usr/lib/ssl/openssl.cnf"
+home = "/home/remnux"
 subprocess.call(["mkdir", "demoCA"])
-subprocess.call(["mkdir", dir+ "/" +"certs"])
-subprocess.call(["mkdir", dir+"/" +"newcerts"])
+subprocess.call(["mkdir", Certs_dir])
+subprocess.call(["mkdir", crl_dir])
+subprocess.call(["mkdir", new_certs_dir])
+subprocess.call(["touch", database])
+#subprocess.call(["echo", "1000"+ >> + serial])
+subprocess.call(["cp", opensslcnf, home])
